158e80ff1149fe03d9c415d3d7613947f66f833a
Krystian <k.szot@nextrope.com>
2020-08-27 16:51:18 +0200

    Update webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -37,11 +37,12 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
-        use: ['file-loader'],
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ['file-loader'],
+        use: [
+          {
+            loader: 'file-loader',
+            options: {},
+          },
+        ],
       },
       {
         test: /\.m?js$/,
